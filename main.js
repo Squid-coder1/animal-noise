42b105ad71158bef3feb8cea8f658d8775854e18
Squid-coder1 <90101725+Squid-coder1@users.noreply.github.com>
2022-12-01 19:48:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,31 @@ function gotResults(error,results) {
     if (error) {
         console.error(error);
     } else {
-        console.log(results);
+        console.log(results); 
+        random_number_r=Math.floor(Math.random()*255)+1;
+        random_number_g=Math.floor(Math.random()*255)+1;
+        random_number_b=Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_label").innerHTML='i can hear-'+results[0].
+        label;
+        document.getElementById("result_confidence").innerHTML ='Accuracy-'+(results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        
+        img1=document.getElementById("ear");
+        
+        if (results[0].label=="background-noise") {
+            img1.src="ear.png";
+           
+        }  else if(results[0].label=="meow") {
+            img1.src="cat.png";
+           
+        } else if(results[0].label=="bark") {
+            img1.src="dog.jpg";
+          
+        } else if(results[0].label=="roar]"){
+            img1.src="lion.webp";
+           
+        }
     }
-}
+ }
